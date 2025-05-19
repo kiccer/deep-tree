5483c9bb89759a140453df0dc9522483a8bc6e13
kiccer <1072907338@qq.com>
2025-05-19 05:39:18 +0000

    ✨ feat: 更新版本号至1.0.4，并优化类型定义的导出方式

--- types.d.ts
+++ types.d.ts
@@ -1,47 +1,49 @@
 // types.d.ts
 
-export type TreeNode = {
-  [key: string]: any;
-};
+type TreeNode = {
+  [key: string]: any
+}
 
-export type DeepFilter = (
-  list: TreeNode[],
-  callback: (item: TreeNode, parent?: TreeNode) => boolean,
-  children?: string,
-  parent?: TreeNode
-) => TreeNode[];
+declare module 'deep-tree' {
+    export const deepFilter: (
+      list: TreeNode[],
+      callback: (item: TreeNode, parent?: TreeNode) => boolean,
+      children?: string,
+      parent?: TreeNode
+    ) => TreeNode[]
 
-export type DeepSome = (
-  list: TreeNode[],
-  callback: (item: TreeNode, parent?: TreeNode) => boolean,
-  children?: string,
-  parent?: TreeNode
-) => boolean;
+    export const deepSome: (
+      list: TreeNode[],
+      callback: (item: TreeNode, parent?: TreeNode) => boolean,
+      children?: string,
+      parent?: TreeNode
+    ) => boolean
 
-export type DeepEvery = (
-  list: TreeNode[],
-  callback: (item: TreeNode, parent?: TreeNode) => boolean,
-  children?: string,
-  parent?: TreeNode
-) => boolean;
+    export const deepEvery: (
+      list: TreeNode[],
+      callback: (item: TreeNode, parent?: TreeNode) => boolean,
+      children?: string,
+      parent?: TreeNode
+    ) => boolean
 
-export type DeepFind = (
-  list: TreeNode[],
-  callback: (item: TreeNode, parent?: TreeNode) => boolean,
-  children?: string,
-  parent?: TreeNode
-) => TreeNode | undefined;
+    export const deepFind: (
+      list: TreeNode[],
+      callback: (item: TreeNode, parent?: TreeNode) => boolean,
+      children?: string,
+      parent?: TreeNode
+    ) => TreeNode | undefined
 
-export type DeepEach = (
-  list: TreeNode[],
-  callback: (item: TreeNode, parent?: TreeNode) => void,
-  children?: string,
-  parent?: TreeNode
-) => void;
+    export const deepEach: (
+      list: TreeNode[],
+      callback: (item: TreeNode, parent?: TreeNode) => void,
+      children?: string,
+      parent?: TreeNode
+    ) => void
 
-export type DeepMap = (
-  list: TreeNode[],
-  callback: (item: TreeNode, parent?: TreeNode) => TreeNode,
-  children?: string,
-  parent?: TreeNode
-) => TreeNode[];
+    export const deepMap: (
+      list: TreeNode[],
+      callback: (item: TreeNode, parent?: TreeNode) => TreeNode,
+      children?: string,
+      parent?: TreeNode
+    ) => TreeNode[]
+}
